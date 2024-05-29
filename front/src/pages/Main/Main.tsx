206a39b554fa8f0224b2eafb04f73d5abf423b03
Cheslav Zhuravsky <happylolonly@gmail.com>
2024-05-29 17:45:31 +0700

    fix

--- front/src/pages/Main/Main.tsx
+++ front/src/pages/Main/Main.tsx
@@ -81,6 +81,7 @@ const Main = () => {
               fontSize: 14,
             }}
           >
+            {/* @ts-ignore */}
             {fromAscii(fromBase64(tonProof?.proof?.payload))}
 
             <br />
