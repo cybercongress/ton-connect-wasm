c9809dfc71c40cce8fd611560e147d75908d62c7
Cheslav Zhuravsky <happylolonly@gmail.com>
2024-05-29 12:52:24 +0700

    updates

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -4,10 +4,12 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 import App from "./App.tsx";
 
-const manifestUrl = "https://??????/config.json";
+// const manifestUrl = "https://nextonserver.s3.eu-north-1.amazonaws.com/config.json";
+const manifestUrl =
+  "https://ton-wasm-cyber-hackathon.netlify.app/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </TonConnectUIProvider>,
+  </TonConnectUIProvider>
 );
