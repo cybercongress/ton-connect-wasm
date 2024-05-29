da876c8c36264f075440402ae44b1c40bf5f422e
Cheslav Zhuravsky <happylolonly@gmail.com>
2024-05-29 13:33:56 +0700

    add passport loadin

---
+++ front/src/hooks/useCyberPassport.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+import { toAscii, toBase64 } from "@cosmjs/encoding";
+
+import axios from "axios";
+
+export async function getPassport(query) {
+  const response = await axios.get(
+    `${"https://lcd.bostrom.cybernode.ai"}/cosmwasm/wasm/v1/contract/${"bostrom1xut80d09q0tgtch8p0z4k5f88d3uvt8cvtzm5h3tu3tsy4jk9xlsfzhxel"}/smart/${toBase64(
+      toAscii(JSON.stringify(query))
+    )}`
+  );
+  return response.data.data;
+}
+
+function useCyberPassport({ nickname }) {
+  const [data, setData] = useState();
+
+  function fetchData() {
+    const query = {
+      passport_by_nickname: {
+        nickname: nickname,
+      },
+    };
+
+    getPassport(query)
+      .then((res) => {
+        setData(res);
+      })
+      .catch((e) => {
+        console.log(e);
+
+        setData(null);
+      });
+  }
+
+  //   useEffect(() => {
+
+  //   }, [nickname]);
+
+  return {
+    fetchData,
+    data,
+  };
+}
+
+export default useCyberPassport;
--- front/src/pages/Main/Main.tsx
+++ front/src/pages/Main/Main.tsx
@@ -4,13 +4,20 @@ import styled from "styled-components";
 import Header from "@/components/common/Header";
 import TonWallet from "@/components/main/TonWallet";
 import useTonConnect from "@/hooks/contract/useTonConnect";
+import useCyberPassport from "@/hooks/useCyberPassport";
 
 const tele = (window as any).Telegram.WebApp;
 
 const Main = () => {
   const { address, tonConnectUI, wallet } = useTonConnect();
 
+  const [nickname, setNickname] = useState("congress");
+
   const tonProof = wallet?.connectItems?.tonProof;
+
+  const { data: passport, fetchData } = useCyberPassport({
+    nickname,
+  });
 
   useEffect(() => {
     if (tele) {
@@ -27,6 +34,29 @@ const Main = () => {
         <TonWallet />
 
         <br />
+
+        <Input
+          value={nickname}
+          placeholder="enter passport..."
+          onChange={(e) => {
+            const value = e.target.value;
+
+            setNickname(value);
+          }}
+        />
+
+        <button onClick={fetchData}>Load</button>
+
+        {passport && (
+          <div
+            style={{
+              fontSize: 14,
+              wordBreak: "break-all",
+            }}
+          >
+            {JSON.stringify(passport)}
+          </div>
+        )}
 
         {tonProof && (
           <div
