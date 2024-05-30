4c3a51005a5b5f1d0107219654f5305a35c58a15
dasein <acidpictures@gmail.com>
2024-05-30 12:49:20 +0300

    fix: redirect

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -14,8 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     actionsConfiguration={{
       // returnStrategy: "https://t.me/awesome_cyber_bot",
       //      // returnStrategy: "https://ton-wasm-cyber-hackathon.netlify.app",
-      returnStrategy: "https://ton-wasm-cyber-hackathon.netlify.app",
+      // returnStrategy: "https://ton-wasm-cyber-hackathon.netlify.app",
       twaReturnUrl: "https://t.me/awesome_cyber_bot/tonconnectwasm",
+      skipRedirectToWallet: "never",
     }}
   >
     <App />
